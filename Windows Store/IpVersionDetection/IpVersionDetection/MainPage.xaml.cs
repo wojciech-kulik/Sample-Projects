f1bc58ca5dbcc4a5397ea571b57627d24b5bdc0a
Wojciech Kulik <wkulik91@gmail.com>
2015-11-27 20:31:07 +0100

    [IpVersionDetection] Small refactoring

--- Windows Store/IpVersionDetection/IpVersionDetection/MainPage.xaml.cs
+++ Windows Store/IpVersionDetection/IpVersionDetection/MainPage.xaml.cs
@@ -41,18 +41,27 @@ namespace IpVersionDetection
         {
             try
             {
+                // resolves domain name to IP addresses (may contain several)
                 var endPointPairs = await DatagramSocket.GetEndpointPairsAsync(new HostName("google.com"), "0");
                 if (endPointPairs == null)
                 {
                     return IpVersion.None;
                 }
 
+                // detect which IP version is supported
                 var result = IpVersion.None;
                 foreach (var endPoint in endPointPairs)
                 {
-                    if (endPoint.RemoteHostName != null && (endPoint.RemoteHostName.Type == HostNameType.Ipv4 || endPoint.RemoteHostName.Type == HostNameType.Ipv6))
+                    if (endPoint.RemoteHostName != null)
                     {
-                        result |= (endPoint.RemoteHostName.Type == HostNameType.Ipv4 ? IpVersion.IPv4 : IpVersion.IPv6);
+                        if (endPoint.RemoteHostName.Type == HostNameType.Ipv4)
+                        {
+                            result |= IpVersion.IPv4;
+                        }
+                        else if (endPoint.RemoteHostName.Type == HostNameType.Ipv6)
+                        {
+                            result |= IpVersion.IPv6;
+                        }
                     }
                 }
                 return result;
